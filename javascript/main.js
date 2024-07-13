438f05e07efccf4e9551b462ec212d22e082c4ef
refu <nnk.refu1516@outlook.jp>
2024-07-13 17:42:38 +0900

    Create main.js

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+// main.js
+
+// DOMContentLoadedイベントが発生した時の処理
+document.addEventListener('DOMContentLoaded', function () {
+    // メニューボタンとナビゲーションメニューの要素を取得
+    var menuBtn = document.getElementById('menu-btn');
