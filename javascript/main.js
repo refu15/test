8410e3831793aba60fe870a411459ab93e66c1dc
NomalHuman1104 <115606577+NomalHuman1104@users.noreply.github.com>
2024-07-14 16:58:10 +0900

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuBtn = document.getElementById('menu-btn');
-    const mobileNav = document.querySelector('.mobile-nav');
+    const mobileNav = document.querySelector('.mobile-header nav');
 
     menuBtn.addEventListener('click', function() {
         mobileNav.classList.toggle('active');
